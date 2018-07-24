e486d5179188b1e7e3aa7d4e0e47a121a277d671
root <root@198.105.244.228>
2018-07-23 21:02:26 -0500

    loop detect

--- bfs.cc
+++ bfs.cc
@@ -13,6 +13,7 @@ class Graph
 {
 private:
     int V;    // No. of vertices
+    int Loop;
  
     // Pointer to an array containing adjacency lists
     list<int> *adj;   
@@ -31,12 +32,18 @@ public:
 
     // DFS traversal of the vertices reachable from v
     void DFS(int v);
+
+    bool isCyclic()
+    {
+    	return this->Loop > 0 ? true:false;
+    };
 };
  
 Graph::Graph(int V)
 {
     this->V = V;
     adj = new list<int>[V];
+    Loop = 0;
 }
  
 void Graph::addEdge(int v, int w)
@@ -99,6 +106,10 @@ void Graph::DFSUtil(int v, bool visited[])
         {
             DFSUtil(i, visited);
         }
+        else
+        {
+        	Loop++;
+        }
     }
 }
  
@@ -109,7 +120,9 @@ void Graph::DFS(int v)
     // Mark all the vertices as not visited
     bool *visited = new bool[V];
     for (int i = 0; i < V; i++)
-        visited[i] = false;
+    {
+    	visited[i] = false;
+    }
  
     // Call the recursive helper function
     // to print DFS traversal
@@ -136,7 +149,7 @@ int main()
     cout << "Following is Depth First Traversal"
         " (starting from vertex 2) \n";
     g.DFS(2);
-    cout << "\n";
+    g.isCyclic()? cout << "Has loop?" <<  "Yes\n": cout << "Has loop?" << "No" << "\n";
  
     return 0;
 }
