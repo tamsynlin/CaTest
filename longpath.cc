39f7ace8e59f6ec8d3295118c3ff3913acafdcf2
root <root@ec2-34-250-90-28.eu-west-1.compute.amazonaws.com>
2018-08-22 23:44:53 -0500

    update

--- longpath.cc
+++ longpath.cc
@@ -101,6 +101,46 @@ int finLongestOverAll(int *mat, int row, int column)
     return longest;
 }
 
+int min(int x, int y, int z);
+
+int minCost(int cost[3][3], int m, int n)
+{
+     int i, j;
+
+     // Instead of following line, we can use int tc[m+1][n+1] or
+     // dynamically allocate memoery to save space. The following line is
+     // used to keep te program simple and make it working on all compilers.
+     int tc[3][3];
+
+     tc[0][0] = cost[0][0];
+
+     /* Initialize first column of total cost(tc) array */
+     for (i = 1; i <= m; i++)
+        tc[i][0] = tc[i-1][0] + cost[i][0];
+
+     /* Initialize first row of tc array */
+     for (j = 1; j <= n; j++)
+        tc[0][j] = tc[0][j-1] + cost[0][j];
+
+     /* Construct rest of the tc array */
+     for (i = 1; i <= m; i++)
+        for (j = 1; j <= n; j++)
+            tc[i][j] = min(tc[i-1][j-1],
+                           tc[i-1][j],
+                           tc[i][j-1]) + cost[i][j];
+
+     return tc[m][n];
+}
+
+/* A utility function that returns minimum of 3 integers */
+int min(int x, int y, int z)
+{
+   if (x < y)
+      return (x < z)? x : z;
+   else
+      return (y < z)? y : z;
+}
+
 int main()
 {
     int n = 3;
@@ -113,5 +153,7 @@ int main()
         << finLongestOverAll((int *)mat, n, n)
         << endl;
     
+    printf("\n\n %d ", minCost(mat, 2, 2));
+
     return 0;
 }
